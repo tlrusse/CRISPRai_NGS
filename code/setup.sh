53123ebb9685d53cd8cf5b50d29737ddc8fdd73b
craut <chinmay0raut@gmail.com>
2026-02-03 12:15:25 -0500

    cleaned up the setup code

--- code/setup.sh
+++ code/setup.sh
@@ -3,11 +3,30 @@
 # run from the code directory with the following cmd:
 # $ . ./setup.sh
 # it will create the environment variables PROJECT_LOC & DATA_OUT in the current shell
+
+PROJ_ORIGINATOR="tlrusse"
+export PROJ_ORIGINATOR
+
 cd ..
 PROJECT_LOC=$(pwd)
 export PROJECT_LOC
-cd code
-mkdir -p /mnt/speliotes-lab/Projects/UK_ATLAS/IndivProj/${USER}
-DATA_OUT=/mnt/speliotes-lab/Projects/UK_ATLAS/IndivProj/${USER}/CRISPRai_NGS/
+
+DATA_IN="/nfs/corenfs/INTMED-Speliotes-data/Projects/15107-PP/"
+export DATA_IN
+mkdir -p /mnt/speliotes-lab/Projects/UK_ATLAS/IndivProj/${PROJ_ORIGINATOR}
+DATA_OUT=/mnt/speliotes-lab/Projects/UK_ATLAS/IndivProj/${PROJ_ORIGINATOR}/CRISPRai_NGS/
 export DATA_OUT
-mkdir -p /mnt/speliotes-lab/Projects/UK_ATLAS/IndivProj/${USER}/CRISPRai_NGS/
+mkdir -p /mnt/speliotes-lab/Projects/UK_ATLAS/IndivProj/${PROJ_ORIGINATOR}/CRISPRai_NGS/
+
+
+# if the following symlinks do not exist, create them
+DATA_IN_LINK=./data_in
+if [ ! -L $DATA_IN_LINK ]; then
+    ln -s $DATA_IN $DATA_IN_LINK
+fi
+DATA_OUT_LINK=./data_out
+if [ ! -L $DATA_OUT_LINK ]; then
+    ln -s /mnt/speliotes-lab/Projects/UK_ATLAS/IndivProj/${PROJ_ORIGINATOR}/CRISPRai_NGS/ $DATA_OUT_LINK
+fi
+
+cd code
